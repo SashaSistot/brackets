fe5214c236505677929469b3c65403f9ec2a6f55
Sasha Sistot <sistot.mytishchi@yandex.ru>
2023-03-01 06:58:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,44 @@
-module.exports = function check(str, bracketsConfig) {
+{
   let flag = (str.length % 2 !== 0) ? false : (checkBracketsInStr(str.split(''), bracketsConfig));
   return flag;
 }
 
 function checkBracketsInStr(arr, bracketsConfig) {
-  for (let i = 0; i < bracketsConfig.length; i++) {
 
-    if (bracketsConfig[i][0] == bracketsConfig[i][1]) {
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    
+    if (bracketsConfig[i][0] === bracketsConfig[i][1]) {
 
       let equalBrackets = arr.filter(element => (element === bracketsConfig[i][0]));
       bracketsConfig.splice(i, 1);
       if (equalBrackets.length % 2 !== 0) return false;
       
-    }
-  
-  }
-  let currentBrackets = 0;
-  for (let j = 0; j < arr.length; j++) {
-    for (let i = 0; i < bracketsConfig.length; i++) {
-      if (arr[j] === bracketsConfig[i][0]) currentBrackets++;
-      if (arr[j] === bracketsConfig[i][1]) currentBrackets--;
-      if (currentBrackets < 0) return false;
-    }
-  }
-  if (currentBrackets !== 0) return false;
+    };
+  };
+
+  let stack = [];
+
+  for (i = 0; i < arr.length; i++) {
+    
+    for (j = 0; j < bracketsConfig.length; j++) {
+      
+      if (arr[i] === bracketsConfig[j][0]) {
+        
+        stack.push(arr[i]);
+        
+      };
+      if (arr[i] === bracketsConfig[j][1] && stack.length === 0) {
+        
+        return false;
+        
+      };
+      if (arr[i] === bracketsConfig[j][1] && stack[stack.length - 1] === bracketsConfig[j][0]) {
+        
+        stack.pop();
+        
+      };
+    };
+  };
+  if (stack.length !== 0) return false;
   return true;
-}
+};
